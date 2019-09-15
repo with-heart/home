448b2a4d2bb3520ec66962ec047f5f0a103ff3b2
Mark Chandler <mark@chanchan.io>
2019-09-15 13:18:02 -0400

    zsh/configs/options: remove autocd opt

--- .zsh/configs/options.zsh
+++ .zsh/configs/options.zsh
@@ -1,6 +1,8 @@
-setopt autocd autopushd pushdminus pushdsilent pushdtohome cdablevars
+setopt autopushd pushdminus pushdsilent pushdtohome cdablevars
 DIRSTACKSIZE=5
 
 setopt extendedglob
+
+setopt IGNORE_EOF
 
 unsetopt nomatch
