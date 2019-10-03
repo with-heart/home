3c22067b0d6dea4a36faabe5a66b60e068459d25
Mark Chandler <mark@chanchan.io>
2019-10-03 01:30:45 -0400

    move sudo keep-alive to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,9 @@
 # Ask for administrator password upfront
 sudo -v
 
+# sudo keep-alive
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
