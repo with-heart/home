cd13a15361bb071f3e59e8c8ac952b31d007f322
Mark Chandler <mark@chanchan.io>
2019-10-03 01:24:46 -0400

    add .macos settings script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+# Ask for administrator password upfront
+sudo -v
+
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install Brewfile contents
 brew bundle
 
-# Remove outdated versions
-brew cleanup
+# Configure macos defaults
+./.macos
+
